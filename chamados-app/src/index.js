ceba00ebf996041f3907c2f68ca9058dea6bcf9c
Michael Kevin <michaelkmb@hotmail.com>
2024-02-19 17:23:14 -0300

    feat header to quick nav for check;

--- chamados-app/src/index.js
+++ chamados-app/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import AppRoutes from './routes/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <header>
+      <Link to="/login">Entrar</Link>
+      <Link to="/signup">Cadastrar</Link>
+    </header>
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
