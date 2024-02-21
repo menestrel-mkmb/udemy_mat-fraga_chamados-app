cdc07307e2f2f8f66c3982d32fc20873756c0b41
Michael Kevin <michaelkmb@hotmail.com>
2024-02-20 21:17:36 -0300

    misc firebaseConfig;

---
+++ chamados-app/src/services/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "chamados-app-2eb4c.firebaseapp.com",
+  projectId: "chamados-app-2eb4c",
+  storageBucket: "chamados-app-2eb4c.appspot.com",
+  messagingSenderId: "927739817409",
+  appId: "1:927739817409:web:b03e91a19f1eb4f69b1340"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseDb = getFirestore(firebaseApp);
