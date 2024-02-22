4aedcb1aa983a1f754bb48267993f393e7b7f801
Michael Kevin <michaelkmb@hotmail.com>
2024-02-22 17:16:46 -0300

    misc using Header Component on index;

--- chamados-app/src/index.js
+++ chamados-app/src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/router';
+
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <header>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/login">Entrar</Link>
-      <Link to="/signup">Cadastrar</Link>
-    </header>
+      <Header />
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
