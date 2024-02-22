9397ea773fe56c96021f39f318fe7849159c0d9d
Michael Kevin <michaelkmb@hotmail.com>
2024-02-22 17:16:11 -0300

    feat Header Component;

---
+++ chamados-app/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Header(){
+    return(
+    <header>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/login">Entrar</Link>
+      <Link to="/signup">Cadastrar</Link>
+    </header>
+    );
+}
