87a752b6afa16b40b2bb1f329f56c2ce3cc5412c
Michael Kevin <michaelkmb@hotmail.com>
2024-02-22 17:40:50 -0300

    feat create User Context to contexts module;

---
+++ chamados-app/src/contexts/auth.js
@@ -0,0 +1,73 @@
+import {
+    createContext,
+    useState
+} from 'react';
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "firebase/auth";
+import { firebaseAuth } from "../services/firebaseConfig";
+
+export const AuthContext = createContext({});
+
+export default function AuthProvider({ children }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [name, setName] = useState('');
+    
+    const [user, setUser] = useState({});
+
+    const loginUser = async (email, password) => {
+
+        if(email === '' || password === '' ||
+            !email || !password
+        ) return;
+        
+        await signInWithEmailAndPassword(firebaseAuth, email, password)
+        .then( (value) => {
+            console.log("Logado com sucesso");
+            setUser(value);
+            console.log(user);
+
+        })
+        .catch( (reason) => {
+            console.log("Erro ao logar");
+            console.log(reason);
+        });
+    }
+
+    const createAccount = async (e)=>{
+        e.preventDefault();
+
+        if(name === '' || email === '' || password === '') return;
+
+        await createUserWithEmailAndPassword(firebaseAuth, email, password)
+        .then( (value) => {
+            console.log("Conta criada com sucesso");
+            console.log(value);
+        })
+        .catch( (reason) => {
+            console.log("Erro ao criar a conta");
+            console.log(reason);
+        })
+    };
+
+    return(
+        <AuthContext.Provider 
+            value={{
+                email, setEmail,
+                password, setPassword,
+                loginUser,
+
+                name, setName,
+                createAccount,
+
+                user, setUser,
+            }}
+        >
+            { children }
+        </AuthContext.Provider>
+    );
+}
