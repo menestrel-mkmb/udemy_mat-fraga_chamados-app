6541315ef3b3ee3cd18f05c822b06733bb6d3ce9
Michael Kevin <michaelkmb@hotmail.com>
2024-02-24 21:01:43 -0300

    misc ToastContainer;

--- chamados-app/src/index.js
+++ chamados-app/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/router';
 import AuthProvider from "./contexts/auth";
@@ -11,6 +14,7 @@ import Header from './components/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <Header />
       <AuthProvider>
