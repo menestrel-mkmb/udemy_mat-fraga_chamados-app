adc1aebddb7045dd5d3d88540db50c76e7a57e63
Michael Kevin <michaelkmb@hotmail.com>
2024-02-24 21:01:57 -0300

    misc toast notify helloworld;

--- chamados-app/src/pages/Dashboard/index.js
+++ chamados-app/src/pages/Dashboard/index.js
@@ -1,7 +1,12 @@
+import { toast } from "react-toastify";
+
 export default function Dashboard(){
+    const notify = () => toast("Teste");
+
     return(
     <main>
         <h2>Dashboard</h2>
+        <button onClick={notify}>Notificação</button>
     </main>
     );
 }
