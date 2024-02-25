4a20a1771b4ec6eeb5985a4fcc83d5bdf21068b1
Michael Kevin <michaelkmb@hotmail.com>
2024-02-24 21:06:30 -0300

    misc remove helloWorld;

--- chamados-app/src/pages/Dashboard/index.js
+++ chamados-app/src/pages/Dashboard/index.js
@@ -1,12 +1,8 @@
-import { toast } from "react-toastify";
-
 export default function Dashboard(){
-    const notify = () => toast("Teste");
 
     return(
     <main>
         <h2>Dashboard</h2>
-        <button onClick={notify}>Notificação</button>
     </main>
     );
 }
