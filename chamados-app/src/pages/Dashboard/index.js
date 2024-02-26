7e0146f9d530315576c9a3edfbc627fbe444fffe
Michael Kevin <michaelkmb@hotmail.com>
2024-02-26 00:01:32 -0300

    feat Dashboard Page with logout btn;

--- chamados-app/src/pages/Dashboard/index.js
+++ chamados-app/src/pages/Dashboard/index.js
@@ -1,8 +1,26 @@
+import { signOut } from "firebase/auth";
+import { firebaseAuth } from "../../services/firebaseConfig";
+
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard(){
+    const { user, setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const logout = async (e) => {
+        e.preventDefault();
+        signOut(firebaseAuth);
+        setUser(null);
+        navigate('/login');
+    }
 
     return(
     <main>
         <h2>Dashboard</h2>
+        <button onClick={e => logout(e)}>Sair</button>
     </main>
     );
 }
