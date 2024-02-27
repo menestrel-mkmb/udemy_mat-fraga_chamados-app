179839ef504a54099bf05c861f297d5854b533c2
Michael Kevin <michaelkmb@hotmail.com>
2024-02-27 19:40:07 -0300

    fix typo;

---
+++ chamados-app/src/pages/Customers/index.js
@@ -0,0 +1,14 @@
+import Wrapper from "../../components/Wrapper";
+
+export default function Customers(){
+    return(
+        <Wrapper
+        className="wrapper"
+    >
+        <main
+            className="main"
+        >
+            <h2>Customers page</h2>
+        </main>
+    </Wrapper>);
+}
