813c23386cf453de3aaec4c0f0ff7c701333c236
Michael Kevin <michaelkmb@hotmail.com>
2024-02-27 19:34:20 -0300

    feat Costumers Page;WIP

---
+++ chamados-app/src/pages/Costumers/index.js
@@ -0,0 +1,13 @@
+import Aside from "../../components/Aside";
+
+export default function Costumers(){
+    return(
+    <>
+    <Aside />
+    <main
+        className="main"
+    >
+        <h2>Costumers page</h2>
+    </main>
+    </>);
+}
