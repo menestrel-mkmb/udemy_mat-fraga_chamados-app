46d03b5471e981c95e3abbdb212754a7e0a04085
Michael Kevin <michaelkmb@hotmail.com>
2024-02-27 19:34:28 -0300

    feat Profile Page;WIP

---
+++ chamados-app/src/pages/Profile/index.js
@@ -0,0 +1,13 @@
+import Aside from "../../components/Aside";
+
+export default function Profile(){
+    return(
+    <>
+    <Aside />
+    <main
+        className="main"
+    >
+        <h2>Profile page</h2>
+    </main>
+    </>);
+}
