ae69d3cbce1709e747a93cd979f82190cd058d61
Michael Kevin <michaelkmb@hotmail.com>
2024-02-27 15:24:14 -0300

    misc structure to css classes; fix Icon inside Link;

--- chamados-app/src/components/Aside/index.js
+++ chamados-app/src/components/Aside/index.js
@@ -1,48 +1,88 @@
 import { Link } from "react-router-dom";
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+import "./index.css";
 
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
-import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
 
 export default function Aside(){
     const { user } = useContext(AuthContext);
 
     return(
-    <aside>
-        <section>
-          <figure>
-            <img
-              src={user.avatarUrl === null ?
-                "../../../assets/imgs/avatar.png" :
-                user.avatarUrl}
-              alt="Avatar/Foto do usuário"
-            />
-            <figcaption>{user.name}</figcaption>
-          </figure>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/dashboard'>
-                  <FiHome color="#FFF" size={24} />
+    <aside
+      className="aside menu__aside"
+    >
+          <div
+            className="avatar__div"
+          >
+            <figure
+              className="avatar__fig"
+            >
+              <img
+                className="avatar__img"
+                src={user.avatarUrl === null ?
+                  "../../../assets/imgs/avatar.png" :
+                  user.avatarUrl}
+                alt="Avatar/Foto do usuário"
+              />
+              <figcaption
+                className="avatar__caption"
+              >
+                {user.name}
+              </figcaption>
+            </figure>
+          </div>
+          <nav
+            className="nav__aside"
+          >
+            <ul
+              className="nav__ul"
+            >
+              <li
+                className="nav__li"
+              >
+                <FiHome
+                  className="li__icon dashboard__icon"
+                  color="#FFF" size={24}
+                />
+                <Link
+                  className="li__lnk dashboard__lnk"
+                  to='/dashboard'
+                >
                   Chamados
                 </Link>
               </li>
-              <li>
-                <Link to='/customers'>
-                  <FiUser color="#FFF" size={24} />
+              <li
+                className="nav__li"
+              >
+                <FiUser
+                    className="li__icon customers__icon"
+                    color="#FFF" size={24}
+                  />
+                <Link
+                  className="li__lnk customers__lnk"
+                  to='/customers'
+                >
                   Clientes
                 </Link>
               </li>
-              <li>
-                <Link to='/profile'>
-                  <FiSettings color="#FFF" size={24} />
+              <li
+                className="nav__li"
+              >
+                <FiSettings
+                  className="li__icon profile__icon"
+                  color="#FFF" size={24}
+                />
+                <Link
+                  className="li__lnk profile__lnk"
+                  to='/profile'
+                >
                   Configurações
                 </Link>
               </li>
             </ul>
           </nav>
-        </section>
       </aside>
     );
 }
