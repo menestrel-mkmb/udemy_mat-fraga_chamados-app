fe5036ddf75494a0e6724c21686e60e2d5bc9cee
Michael Kevin <michaelkmb@hotmail.com>
2024-02-27 14:08:18 -0300

    misc Header class for style;

--- chamados-app/src/components/Header/index.js
+++ chamados-app/src/components/Header/index.js
@@ -1,12 +1,34 @@
 import { Link } from "react-router-dom";
+import "./index.css";
 
 export default function Header(){
 
   return(
-  <header>
-    <Link to="/dashboard">Dashboard</Link>
-    <Link to="/login">Entrar</Link>
-    <Link to="/signup">Cadastrar</Link>
+  <header
+    className="header"
+  >
+    <nav
+      className="menu menu__nav"
+    >
+      <Link
+        className="menu__lnk dashboard__lnk"
+        to="/dashboard"
+      >
+        Dashboard
+      </Link>
+      <Link
+        className="menu__lnk login__lnk"
+        to="/login"
+      >
+        Entrar
+      </Link>
+      <Link
+        className="menu__lnk signup__link"
+        to="/signup"
+      >
+        Cadastrar
+      </Link>
+    </nav>
   </header>
   );
 }
