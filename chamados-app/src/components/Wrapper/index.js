add19dc5adef5760d3bb0677fc06636cb035e73a
Michael Kevin <michaelkmb@hotmail.com>
2024-02-29 15:36:47 -0300

    fix volatile margin for web responsiveness;

--- chamados-app/src/components/Wrapper/index.js
+++ chamados-app/src/components/Wrapper/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 export default function Wrapper({ children }){
     return(
         <>
-        <Aside />
         <section
             className="wrapper"
         >
+            <Aside />
             {children}
         </section>
         </>
