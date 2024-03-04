c7b974788578a68186e78646fd26b72339895578
Michael Kevin <michaelkmb@hotmail.com>
2024-03-04 11:06:51 -0300

    feat form for customerInfo;

--- chamados-app/src/pages/Customers/index.js
+++ chamados-app/src/pages/Customers/index.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+// import "./index.css";
+
+import Wrapper from "../../components/Wrapper";
 import Main from "../../components/Main";
 import Title from "../../components/Title";
-import Wrapper from "../../components/Wrapper";
 import { FiUser } from "react-icons/fi";
 
 export default function Customers(){
+    const [customerName, setCustomerName] = useState('');
+    const [customerCnpj, setCustomerCnpj] = useState('');
+    const [customerAddress, setCustomerAddress] = useState('');
+
+    const handleNewClient = async (e) => {
+        e.preventDefault();
+    }
+
     return(
         <Wrapper
         className="wrapper"
@@ -15,6 +26,74 @@ export default function Customers(){
                 <FiUser size={24} />
                 Customers
             </Title>
+            <form
+                className="customers__form"
+                onSubmit={e=>handleNewClient(e)}
+            >
+                <section
+                    className="inp__sect customer-name customer-name__sect"
+                >
+                    <label
+                        className="customer-name__lbl lbl"
+                        htmlFor="customer-name"
+                    >
+                        Nome da empresa
+                    </label>
+                    <input
+                        className="customer-name__inp inp"
+                        type="text"
+                        customerName="customer-ame"
+                        placeholder="Nome do cliente"
+                        required
+                        value={customerName}
+                        onChange={e=>setCustomerName(e.target.value)}
+                    />
+                </section>
+                <section
+                    className="inp__sect cnpj cnpj__sect"
+                >
+                    <label
+                        className="cnpj__lbl lbl"
+                        htmlFor="cnpj"
+                    >
+                        CNPJ
+                    </label>
+                    <input
+                        className="cnpj__inp inp"
+                        type="text"
+                        name="cnpj"
+                        placeholder="CNPJ do cliente"
+                        required
+                        value={customerCnpj}
+                        onChange={e=>setCustomerCnpj(e.target.value)}
+                    />
+                </section>
+                <section
+                    className="inp__sect address address__sect"
+                >
+                    <label
+                        className="address__lbl lbl"
+                        htmlFor="address"
+                    >
+                        Endereço Físico
+                    </label>
+                    <input
+                        className="address__inp inp"
+                        type="text"
+                        name="address"
+                        placeholder="Endereço do cliente"
+                        required
+                        value={customerAddress}
+                        onChange={e=>setCustomerAddress(e.target.value)}
+                    />
+                </section>
+                <button
+                    className="form__btn feature-btn"
+                    type="submit"
+                >
+                    Salvar
+                </button>
+            </form>
         </Main>
     </Wrapper>);
 }
