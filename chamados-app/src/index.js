2a575f90b427bdd0f858bf97876446b2a76011c3
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-03-18 14:07:42 -0300

    misc using TicketProvider on App;

--- chamados-app/src/index.js
+++ chamados-app/src/index.js
@@ -5,12 +5,13 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Header from './components/Header';
+
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/router';
 import AuthProvider from "./contexts/auth";
-
-import Header from './components/Header';
 import CustomerProvider from './contexts/customer';
+import TicketsProvider from './contexts/tickets';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +21,9 @@ root.render(
       <Header />
       <AuthProvider>
       <CustomerProvider>
+      <TicketsProvider>
         <AppRoutes />
+      </TicketsProvider>
       </CustomerProvider>
       </AuthProvider>
     </BrowserRouter>
